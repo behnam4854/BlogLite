0e7bf39f07887ed077396a31ed7ca588bae565ac
ibehnam <behnam4854@gmail.com>
2023-10-15 22:24:35 +0330

    added post list and post detail view

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from core.views import homeview,current_datetime, HomeClassBasedView
+from core.views import PostlistView,PostDetailView
 
 urlpatterns = [
         # path('', homeview, name='home'),
-        path('', HomeClassBasedView.as_view(), name='home'),
-        path('test/', current_datetime, name='test'),
+        path('', PostlistView.as_view(), name='post-list'),
+        path("<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
+
+        # path('test/', testView.as_view(greeting = 'Noooooooooooooo this is better'), name='test'),
     ]
--- app/core/views.py
+++ app/core/views.py
@@ -2,20 +2,43 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods, require_safe
 from django.views import View
-
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Post
 import datetime
-@require_safe
-def homeview(request):
-    """the first version of home view using simple functions"""
-    # return HttpResponse("<h1>Page was found</h1>")
-    return render(request,'base.html')
-async def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-class HomeClassBasedView(View):
-    """this is for home view using generic class based view"""
-    def get(self, request):
-        now = datetime.datetime.now()
-        html = "<html><body>It is now %s.</body></html>" % now
-        return HttpResponse(html)
+
+# @require_safe
+# def homeview(request):
+#     """the first version of home view using simple functions"""
+#     # return HttpResponse("<h1>Page was found</h1>")
+#     return render(request,'base.html')
+# async def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = "<html><bo`dy>It is now %s.</body></html>" % now
+#     return HttpResponse(html)
+# class HomeClassBasedView(View):
+#     """this is for home view using generic class based view"""
+#     def get(self, request):
+#         now = datetime.datetime.now()
+#         html = "<html><body>It is now %s.</body></html>" % now
+#         return HttpResponse(html)
+# class testView(View):
+#     greeting = "hi how are you"
+
+#     def get(self, request):
+#         return HttpResponse(self.greeting)
+
+class PostlistView(ListView):
+    """for retriving all the post in the home view"""
+    model = Post
+    # paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = datetime.datetime.now()
+        return context
+
+class PostDetailView(DetailView):
+    """for views the single post detail view and reviewing it"""
+    model = Post
